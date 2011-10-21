a8246789807aa69f451be7b86e5585df73d6e378
Jacques de Laval <chucky@wrutschkow.org>
2011-10-21 11:41:38 +0200

    Minor fix

--- services.py
+++ services.py
@@ -18,7 +18,7 @@ service = [
 			{	're'		:	r'(?:http://)?(?:www.)?urplay.se/(?P<url>.+)',
 				'template'	:	'http://urplay.se/%(url)s'},
 			{	're'		:	r'file=/(?P<url>[^&]+).*?(?P<ext>mp[34]):',
-				'template'	:	'rtmpdump -r rtmp://streaming.ur.se/ -y %(ext)s:/%(url)s -a ondemand -o %(output_file)s'}],
+				'template'	:	'#RTMP-stream\nrtmpdump -r rtmp://streaming.ur.se/ -y %(ext)s:/%(url)s -a ondemand -o %(output_file)s'}],
 		[#TV4-play
 			{	're'		:	r'(?:http://)?(?:www.)?tv4play.se/.*videoid=(?P<id>\d+).*',
 				'template'	:	'http://premium.tv4play.se/api/web/asset/%(id)s/play'},
@@ -40,7 +40,7 @@ service = [
 			{	're'		:	r'(?:http://)?(?:www.)?aftonbladet.se/(?P<url>.+)',
 				'template'	:	'http://aftonbladet.se/%(url)s'},
 			{	're'		:	'videoUrl:\s"(?P<base>rtmp://(ss11i04.stream.ip-only.net|fl1.c00862.cdn.qbrick.com)/[^/]+/)(?P<url>[^"]+)"',
-				'template'	:	'rtmpdump -r %(base)s -y %(url)s -o %(output_file)s'}],
+				'template'	:	'#RTMP-stream\nrtmpdump -r %(base)s -y %(url)s -o %(output_file)s'}],
 		[#Vimeo
 			{
 				'user-agent-string':	'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/534.36 (KHTML, like Gecko) Chrome/13.0.766.0 Safari/534.36',
@@ -66,4 +66,4 @@ service = [
 			{	're'		:	r'(?:http://)?(?:www.)?axess.se/(?P<url>.+)',
 				'template'	:	'http://axess.se/%(url)s'},
 			{	're'		:	r'url: \'(?P<url>[^\']+)\'.*netConnectionUrl: \'(?P<base>[^\']+)',
-				'template'	:	'rtmpdump -r %(base)s -y %(url)s -o %(output_file)s'}]]
+				'template'	:	'#RTMP-stream\nrtmpdump -r %(base)s -y %(url)s -o %(output_file)s'}]]
